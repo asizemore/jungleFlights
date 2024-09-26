f2f9085be2c21623afe13705374416221b1a2172
asizemore <annsize@seas.upenn.edu>
2024-09-25 21:51:04 -0700

    fixed dependencies and added header

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { render } from 'react-dom';
 import FlightDescriptions from "./components/FlightDescriptions"
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
 
 function App() {
   return (
-    // <Button>Hello tyler</Button>
-    <FlightDescriptions />
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Jungle Flights
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <FlightDescriptions />
+    </div>
     
   )
 }
